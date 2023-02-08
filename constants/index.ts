951877fd7dea43de3ac6837f6f4313383fb82a15
Jeremiah Lena <jeremiahlena13@gmail.com>
2023-02-08 19:15:06 +0100

    remove localhost

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,4 @@
-// export const API_URL = 'http://iroyin-backend-env.eba-wmgpq2d7.us-west-2.elasticbeanstalk.com'
-export const API_URL = 'http://localhost:8000'
+export const API_URL = 'http://iroyin-backend-env.eba-wmgpq2d7.us-west-2.elasticbeanstalk.com'
 export const EMAIL_VALIDATION_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 export const FULL_NAME_REGEX = /\w+ \w+/
 export const USER_TOKEN_KEY = 'READNEWS_USER_TOKEN'
