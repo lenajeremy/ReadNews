0ec5c53b9d2d5fb1d84178673bbf97423ad51ff9
Jeremiah Lena <jeremiahlena13@gmail.com>
2023-02-22 23:48:13 +0100

    fixed sliding bug and added slider

--- components/NewsScreen/Categories.tsx
+++ components/NewsScreen/Categories.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
-import { Pressable } from 'react-native'
 import { Box, PressableWithHaptics, Text } from '../shared'
+import Animated from 'react-native-reanimated'
+
+const AnimatedBox = Animated.createAnimatedComponent(Box)
 
 const Categories: React.FC = () => {
   type CategoryOptions = 'Feeds' | 'Popular' | 'Following'
@@ -37,18 +39,20 @@ const Categories: React.FC = () => {
   }
 
   return (
-    <Box
-      marginVertical="md"
-      borderRadius={20}
-      marginHorizontal={'lg'}
-      backgroundColor="transparentBackground"
-      padding={'xxs'}
-      flexDirection="row"
-      justifyContent="space-between"
-    >
-      {categories.map((category, index) => (
-        <Category title={category} key={index} />
-      ))}
+    <Box backgroundColor='mainBackground'>
+      <AnimatedBox
+        marginBottom="md"
+        borderRadius={20}
+        marginHorizontal={'lg'}
+        backgroundColor="transparentBackground"
+        padding={'xxs'}
+        flexDirection="row"
+        justifyContent="space-between"
+      >
+        {categories.map((category, index) => (
+          <Category title={category} key={index} />
+        ))}
+      </AnimatedBox>
     </Box>
   )
 }
--- components/NewsScreen/News.tsx
+++ components/NewsScreen/News.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { FlatList, Pressable, ActivityIndicator, Alert } from 'react-native'
 import { Box, PressableWithHaptics, Text } from '../shared'
 import NewsComponent from './NewsComponent'
-import LoadingNews from './LoadingNews'
 import Categories from './Categories'
 import FeaturedNews from './FeaturedNews'
 import { getDateText, getTimeOfDay } from '../../utils'
@@ -15,111 +14,45 @@ import type { NewsType } from '../../types'
 import { useAppSelector } from '../../hooks/reduxhooks'
 import { NavigationProp, useNavigation } from '@react-navigation/native'
 import { RootTabParamList } from '../../navigation/types'
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated'
+
+const isNews = (res: any): res is NewsType => {
+  return Boolean(res?.url)
+}
 
 const News = () => {
-  const [allNews, setAllNews] = React.useState<NewsType[]>([])
-  const [pageNumber, setPageNumber] = React.useState<number>(1)
-  const token = useAppSelector((store) => store.user.token)
+  type HeaderElementKeys =
+    | 'greetingbanner'
+    | 'categories'
+    | 'featurednews'
+    | 'titleheader'
+  type NewsTypeWithKey = NewsType | { _key: HeaderElementKeys }
 
-  const [getNewsFromAPI, { isLoading, isFetching }] = useLazyGetNewsQuery()
-  const [registerInteraction] = useRegisterInteractionMutation()
+  const DEFAULT_FLATLIST_HEADER_COMPONENTS: NewsTypeWithKey[] = React.useMemo(() => {
+    return [
+      { _key: 'greetingbanner' },
+      { _key: 'categories' },
+      { _key: 'featurednews' },
+    ]
+  }, [])
 
-  const fetchNews = React.useCallback(
-    async function (_pageNumber?: number) {
-      try {
-        const res = await getNewsFromAPI({
-          page_number: _pageNumber ? _pageNumber : pageNumber,
-        }).unwrap()
-
-        console.log(res)
-
-        setAllNews(
-          _pageNumber === 1 ? [...res.news] : [...allNews, ...res.news],
-        )
-        setPageNumber(res.nextPage)
-      } catch (error) {
-        console.error(error)
-      }
-    },
-    [pageNumber],
+  const [allNews, setAllNews] = React.useState<Array<NewsTypeWithKey>>(
+    DEFAULT_FLATLIST_HEADER_COMPONENTS,
   )
 
-  React.useEffect(() => {
-    if (token) {
-      fetchNews()
-    }
-  }, [token])
-
-  if (isLoading) {
-    return (
-      <Box flex={1} alignItems="center" justifyContent="center">
-        <ActivityIndicator />
-      </Box>
-    )
-  }
-
-  return (
-    <FlatList
-      ListHeaderComponent={FlatListHeaderComponent}
-      ListFooterComponent={() => (isFetching ? <ActivityIndicator /> : null)}
-      ItemSeparatorComponent={() => (
-        <Box
-          height={2}
-          width={'100%'}
-          backgroundColor="transparentBackground"
-        ></Box>
-      )}
-      contentContainerStyle={{
-        paddingBottom: isFetching ? 100 : 80,
-      }}
-      onRefresh={() => fetchNews(1)}
-      refreshing={isFetching}
-      data={allNews}
-      keyExtractor={(item, index) => item.url + index.toString()}
-      onEndReachedThreshold={0.9}
-      onEndReached={() => fetchNews()}
-      renderItem={({ item }) => (
-        <NewsComponent
-          item={item}
-          removeItem={(url) => {
-            setAllNews(allNews.filter((news) => news.url !== url))
-            registerInteraction({ url, action: 'DISLIKE', effect: 'POSITIVE'})
-          }}
-          registerInteraction={(url) =>
-            registerInteraction({ url, action: 'READ', effect: 'POSITIVE' })
-          }
-        />
-      )}
-    />
-  )
-}
-
-const GreetingBanner: React.FC = () => {
-  const { firstName } = useAppSelector((store) => store.user)
-
-  return (
-    <Box paddingHorizontal="lg" paddingVertical="sm">
-      <Text color="mutedText" marginBottom="xs" fontSize={14}>
-        {getDateText()}
-      </Text>
-      <Text variant="heading3" fontFamily="Gilroy-Bold" color="mainText">
-        {getTimeOfDay()}, {'\n'}
-        {firstName}
-      </Text>
-    </Box>
-  )
-}
-
-const FlatListHeaderComponent = () => {
   const navigation = useNavigation<
     NavigationProp<RootTabParamList, 'NewsScreen'>
   >()
 
-  return (
-    <>
-      <GreetingBanner />
-      <Categories />
-      <FeaturedNews />
+  const map: Record<HeaderElementKeys, React.ReactNode> = {
+    greetingbanner: <GreetingBanner />,
+    categories: <Categories />,
+    featurednews: <FeaturedNews />,
+    titleheader: (
       <Box
         flexDirection="row"
         justifyContent="space-between"
@@ -144,8 +77,159 @@ const FlatListHeaderComponent = () => {
           </Text>
         </PressableWithHaptics>
       </Box>
-    </>
+    ),
+  }
+
+  const [pageNumber, setPageNumber] = React.useState<number>(1)
+  const token = useAppSelector((store) => store.user.token)
+
+  const [getNewsFromAPI, { isLoading, isFetching }] = useLazyGetNewsQuery()
+  const [registerInteraction] = useRegisterInteractionMutation()
+
+  const fetchNews = React.useCallback(
+    async function (_pageNumber?: number) {
+      try {
+        const res = await getNewsFromAPI({
+          page_number: _pageNumber ? _pageNumber : pageNumber,
+        }).unwrap()
+
+        setAllNews(
+          _pageNumber === 1
+            ? [...DEFAULT_FLATLIST_HEADER_COMPONENTS, ...res.news]
+            : [...allNews, ...res.news],
+        )
+        setPageNumber(res.nextPage)
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    [pageNumber, allNews],
   )
+
+  React.useEffect(() => {
+    if (token) {
+      fetchNews()
+    }
+  }, [token])
+
+  if (isLoading) {
+    return (
+      <Box flex={1} alignItems="center" justifyContent="center">
+        <ActivityIndicator />
+      </Box>
+    )
+  }
+
+  return (
+    <FlatList
+      stickyHeaderIndices={[1]}
+      ListFooterComponent={() => (isFetching ? <ActivityIndicator /> : null)}
+      ItemSeparatorComponent={({ leadingItem }) =>
+        isNews(leadingItem) ? (
+          <Box
+            height={2}
+            width={'100%'}
+            backgroundColor="transparentBackground"
+          ></Box>
+        ) : null
+      }
+      contentContainerStyle={{
+        paddingBottom: isFetching ? 100 : 80,
+      }}
+      onRefresh={() => fetchNews(1)}
+      refreshing={isLoading}
+      data={allNews}
+      keyExtractor={(item, index) =>
+        isNews(item) ? item.url + index.toString() : item._key
+      }
+      onEndReachedThreshold={0.9}
+      onEndReached={() => fetchNews()}
+      renderItem={({ item }) => {
+        if (isNews(item)) {
+          return (
+            <NewsComponent
+              item={item}
+              removeItem={(url) => {
+                setAllNews(
+                  allNews.filter((news) =>
+                    !isNews(news) ? true : news.url !== url,
+                  ),
+                )
+                registerInteraction({
+                  url,
+                  action: 'DISLIKE',
+                  effect: 'POSITIVE',
+                })
+              }}
+              registerInteraction={(url) =>
+                registerInteraction({ url, action: 'READ', effect: 'POSITIVE' })
+              }
+            />
+          )
+        } else {
+          return map[item._key]
+        }
+      }}
+    />
+  )
+}
+
+const GreetingBanner: React.FC = () => {
+  const { firstName } = useAppSelector((store) => store.user)
+
+  return (
+    <Box paddingHorizontal="lg" paddingVertical="sm">
+      <Text color="mutedText" marginBottom="xs" fontSize={14}>
+        {getDateText()}
+      </Text>
+      <Text variant="heading3" fontFamily="Gilroy-Bold" color="mainText">
+        {getTimeOfDay()}, {'\n'}
+        {firstName}
+      </Text>
+    </Box>
+  )
+}
+
+const FlatListHeaderComponent = ({
+  animatedCategoriesStyles,
+}: {
+  animatedCategoriesStyles: any
+}) => {
+  const navigation = useNavigation<
+    NavigationProp<RootTabParamList, 'NewsScreen'>
+  >()
+
+  const components = [
+    <GreetingBanner />,
+    <Categories />,
+    <FeaturedNews />,
+    <Box
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      marginBottom="md"
+      marginTop="lg"
+      marginHorizontal="lg"
+    >
+      <Text
+        variant="heading3"
+        fontSize={20}
+        fontFamily="Gilroy-Bold"
+        color="mainText"
+      >
+        Just For You
+      </Text>
+      <PressableWithHaptics
+        onPress={() => navigation.navigate('ExploreScreen')}
+      >
+        <Text style={{ color: '#2b7efe' }} fontFamily="Gilroy-Bold">
+          See More
+        </Text>
+      </PressableWithHaptics>
+    </Box>,
+  ]
+
+  return components
 }
 
 export default News
